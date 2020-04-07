6cdf5a74bd8594a0517b8fa9d8fbf470ee29261a
无风引漩 <scirocco_gti@yeah.net>
2020-04-07 12:43:21 +0800

    feat: !!day set

--- daycount.py
+++ daycount.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import copy
 import datetime
+import re
 
 startday = datetime.datetime.strptime('2018-11-09', '%Y-%m-%d')
 
@@ -10,9 +11,16 @@ def onServerInfo(server, info):
       server.say('今天是这个服务器开服的第' + getday() + '天')
 
 def on_info(server, info):
-  info2 = copy.deepcopy(info)
-  info2.isPlayer = info2.is_player
-  onServerInfo(server, info2)
+  if info.content.startswith('!!day set'):
+    try:
+      newstartstr = re.match(r'!!day set (\S*)').groups()[0]
+      newstartday = datetime.datetime.strptime(newstartstr, '%Y-%m-%d')
+    except:
+      server.reply(info, '§cPlease enter start day as yyyy-mm-dd')
+  else:
+    info2 = copy.deepcopy(info)
+    info2.isPlayer = info2.is_player
+    onServerInfo(server, info2)
 
 def getday():
   now = datetime.datetime.now()
